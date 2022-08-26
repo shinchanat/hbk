1d96f11c963326c0a8102a8d650018e56a4f1d5b
Harish <93109277+Harish-developments@users.noreply.github.com>
2022-08-26 23:04:34 +0530

    Add files via upload

--- htmlTemplate.py
+++ htmlTemplate.py
@@ -1,10 +1,11 @@
-
 class Html:
-    def __init__(self,components = "helloworld"):
+    
+    def __init__(self,root = None):
         self.template = (
-            f"""<html>\n\t<head>\n\t\t<title>\n\t\t</title>\n\t\
-            <link rel = "stylesheet" href = "index.css">\n\t</head>\
-            \n<body>\n\t{components}\n</body>\n</html>""")
+            f"""<html>\n\t<head>\n\t\t<title></title>\n\t\
+        <link rel = "stylesheet" href = "index.css">\n\t</head>\
+            \n<body>\n\t{root.self_generate()}\n</body>\n</html>"""
+            )
 
 
 
@@ -17,19 +18,27 @@ class Html:
 if __name__ == "__main__":
     from button import Button
     from input import Input
+    from boxlayout import BoxLayout
 
-    field = Input(radius = (20,)*4,bgcolor = "skyblue",color = "black",pos = ("200px","200px"),
-                  placeholder = "Enter your name : ",
-                  padding = (30,)*4)
-    
-    html = Html(field.self_generate())
-    css = field.style_generate()
-    print(css)
+    root = BoxLayout()
+    layout = BoxLayout(xAlign = "flex-start",bgColor = "red")
+    layout2 = BoxLayout(alignBy = "column",bgColor = "green")
+    inp = Input( bgColor = "orange" ,mode = "password")
+    btn = Button()
+    layout.addComponent([inp,btn])
+    layout2.addComponent([inp,btn])
+    root.addComponent([layout,layout2])
+
+    html = Html(root)
+
 
     with open("file.html","w") as htmlfile:
+        
         htmlfile.write(html.template)
 
     with open("index.css","w") as cssfile:
-        cssfile.write(css)
 
-    print(html.template)
+        cssfile.write(root.style_generate())
+
+##    print(layout.self_generate())
+##    print(layout.style_generate())
