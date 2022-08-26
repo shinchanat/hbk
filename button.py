294bf822812a3fa1e6dd8d4424e998e4ef950593
Harish <93109277+Harish-developments@users.noreply.github.com>
2022-08-26 23:02:48 +0530

    Changed the parameters.

--- button.py
+++ button.py
@@ -1,41 +1,41 @@
 class Button:
 
     count = 0
-    def __init__(self,name = "default-button",text = "BUTTON",padding = (10,)*4,
-                 color = "white" ,bgcolor = "blue",size = (None,)*2,radius = 10,
-                 posMode = "absolute",pos = ("0px","0px"),fontSize = "10px",
-                 fontFamily = "Courier New"):
+    def __init__(self,name = "button",text = "BUTTON",padding = {"top":"10px","right":"10px","bottom":"10px","left":"10px"},
+                 color = "white" , bgColor = "blue",size = (None,)*2,radius = 10,posMode = "relative",
+                 pos = ("0px","0px"),fontSize = "10px",fontFamily = "Courier New", elevation = "10px",
+                 margin = {"left":"10px","right":"10px","top":"10px","bottom":"10px"}):
         
-        self.name = name+"-"+str(Button.count) if name == "default-button" else name
+        self.name = name+"-"+str(Button.count) if name == "button" else name
         self.text = text
         self.padding = padding
-        self.color = color
+        self.color = "black" if bgColor == "transparent" else color
         self.size = size
         self.posMode = posMode
-        self.bgcolor = bgcolor
+        self.bgcolor = bgColor
         self.fontFamily = fontFamily
         self.fontSize = fontSize
         self.radius = radius
-        self.buttonCount()
+        self.elevation = elevation
+        self.margin = margin
+        Button.count += 1
         self.pos = pos
-
-
-    def buttonCount(self):
-        Button.count +=1
 
     def style_generate(self):
 
-        return (f".{self.name}"+"{\
+        return (f"\n.{self.name}"+"{\
             \n\tcolor : "+f"{self.color};\n\t"
             "background-color :" + f"{self.bgcolor};\n\t"
-            "padding :" + f"{self.padding[0]}px {self.padding[1]}px {self.padding[2]}px {self.padding[3]}px;\n\t"
+            "padding :" + f"{self.padding.get('top','10px')} {self.padding.get('right','10px')} {self.padding.get('bottom','10px')} {self.padding.get('left','10px')};\n\t"
             "width : " +f"{self.size[0]}px;\n\t"
             "position : "+f"{self.posMode};\n\t"
             "top : "+f"{self.pos[0]};\n\t"
+            "margin : "+f"{self.margin.get('top','10px')} {self.margin.get('right','10px')} {self.margin.get('bottom','10px')} {self.margin.get('left','10px')};\n\t"
             "left : "+f"{self.pos[1]};\n\t"
             "height : " +f"{self.size[1]}px;\n\t"
             "border : " +"none;\n\t"
             "font-size : "+f"{self.fontSize};\n\t"
+            "box-shadow : "+f"{self.elevation};\n\t"
             "font-family : "+f"'{self.fontFamily}';\n\t"
             "border-radius : " +f"{self.radius}px;"+"\n}\n"\
             +f".{self.name}:hover"+"{\n"
@@ -48,11 +48,11 @@ class Button:
 
     def function_generate(self):
 
-        return f""" function onClick_{self.name.replace('-','_')}()"""+"{\n\tconsole.log(\"clicked\");\n}"
+        return f"""\nfunction onClick_{self.name.replace('-','_')}()"""+"{\n\tconsole.log(\"clicked\");\n}"
 
     def self_generate(self):
 
-        return (f"""<button class = "{self.name}">{self.text}</button>""")
+        return (f"""\n<button class = "{self.name}">{self.text}</button>""")
 
 if __name__ == "__main__":
 
